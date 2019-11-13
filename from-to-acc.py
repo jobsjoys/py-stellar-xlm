fbb709f774028e1c4dfbbbbe9325b30fb4e29120
jobsjoys <jobsjoys@users.noreply.github.com>
2019-11-13 22:58:49 +0800

    Donation thermometer interfacing with XLM account

--- from-to-acc.py
+++ from-to-acc.py
@@ -1,11 +1,11 @@
 from stellar_base.builder import Builder
-seed = "SDEYJDC2TP6T7LMF5E2GVEYWHNNM3BFLRLU2HPMXVXHOCWQQVBYFUXBG"
+seed = "SANYOBAAZ26YNCIRMMCBFX6IIDPFLNYA4WTXNTAXWE77XIG5UXMYV5CR"
 builder = Builder(secret=seed)
 # builder = Builder(secret=seed, network='public') for LIVENET
 
 bob_address = 'GAU3DAERZWH4DC4XTUQ5IXYHJHZ67LQ5OCXU5GJJOADIPSJMGELBJAFW'
-builder.append_payment_op(bob_address, '1', 'XLM')
-builder.add_text_memo("#AABBCC") # string length <= 28 bytes
+builder.append_payment_op(bob_address, '696', 'XLM')
+builder.add_text_memo("I AM A LEGEND") # string length <= 28 bytes
 builder.sign()
 
 # Uses an internal horizon instance to submit over the network
--- static/js/application.js
+++ static/js/application.js
@@ -8,7 +8,13 @@ $(document).ready(function(){
         console.log('Balance: '  + msg.balance + ' XLM | Last tx: ' + msg.amount + ' ' + msg.asset_code + ' | Memo: ' + msg.memo);
 
         $('#label').html('Balance: '  + msg.balance + ' XLM | Last tx: ' + msg.amount + ' ' + msg.asset_code + ' | Memo: ' + msg.memo);
-        $('body').css('background-color',msg.memo);
+        //$('body').css('background-color',msg.memo);
+        $('#funded').html(Math.floor((msg.balance/100000)*100) + '%');
+        $('#progress').css('width',Math.floor((msg.balance/100000)*100) + '%');
+        $('#raised').html(msg.balance + ' XLM');
+        $('#last_tx').html(msg.amount + ' ' + msg.asset_code);
+        $('#memo').html(msg.memo);
+
     });
 
 });
